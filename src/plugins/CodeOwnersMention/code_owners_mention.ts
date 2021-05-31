4e5a8db53e931ca6a77b451a2a3f668ee2a2c764
Alex Peters <alex@peters.net>
2021-05-31 13:20:20 +1000

    Trivial adjustments to generated message (#77)
    * its been labeled → it has been labeled
    * codeowner → code owner (source: [GitHub Blog](https://github.blog/2017-07-06-introducing-code-owners/#:~:text=Introducing-,code%20owners,-Jared%20Pace))

--- src/plugins/CodeOwnersMention/code_owners_mention.ts
+++ src/plugins/CodeOwnersMention/code_owners_mention.ts
@@ -95,7 +95,7 @@ export const runCodeOwnersMention = async (
     const triggerLabel = context.name === "issues" ? "issue" : "pull request";
     const commentBody = `Hey there ${mentions.join(
       ", "
-    )}, mind taking a look at this ${triggerLabel} as its been labeled with an integration (\`${integrationName}\`) you are listed as a [codeowner](${codeownersLine}) for? Thanks!`;
+    )}, mind taking a look at this ${triggerLabel} as it has been labeled with an integration (\`${integrationName}\`) you are listed as a [code owner](${codeownersLine}) for? Thanks!`;
 
     log.info(`Adding comment to ${triggerLabel} ${triggerURL}: ${commentBody}`);
 

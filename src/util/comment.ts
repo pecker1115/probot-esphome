home-assistant/probot-home-assistant

Await posting of comments & group messages (#81)

src/util/comment.ts
@@ -3,36 +3,43 @@
  *
  * We debounce it so that we only leave 1 comment with all notices.
  */
-import { debounce } from "debounce";
 import { COMMENT_DEBOUNCE_TIME } from "../const";
 import { PRContext, IssueContext } from "../types";
 import { getIssueFromPayload } from "./issue";
+import { debounce } from "./debounce";
 
 type PendingComment = {
-  debouncedPost: Function,
-  context: PRContext | IssueContext,
-  comments: Array<{ handler: string, message: string }>;
-}
+  debouncedPost: Function;
+  context: PRContext | IssueContext;
+  comments: Array<{ handler: string; message: string }>;
+};
 
 const pendingComments = new Map<string, PendingComment>();
 
-const postComment = (key: string) => {
+const postComment = async (key: string) => {
   const pendingComment = pendingComments.get(key);
   pendingComments.delete(key);
 
   const context = pendingComment.context;
 
-  const toPost = pendingComment.comments.map(
-    (comment) =>
-      `${comment.message}\n<sub><sup>(message by ${comment.handler})</sup></sub>`
+  // Group messages by handler in alphabetical order
+  const handlers = new Set(pendingComment.comments.map((c) => c.handler));
+  const toPost = [...handlers].sort().map(
+    (handler) =>
+      `${pendingComment.comments
+        .filter((comment) => comment.handler === handler)
+        .map((comment) => comment.message)
+        .join("\n")}\n` + `<sub><sup>(message by ${handler})</sup></sub>`
   );
 
-  let commentBody = toPost.join("\n\n---\n\n");
+  const commentBody = toPost.join("\n\n---\n\n");
 
-  context.github.issues.createComment(context.issue({ body: commentBody }));
+  await context.github.issues.createComment(
+    context.issue({ body: commentBody })
+  );
 };
 
-export const scheduleComment = (
+export const scheduleComment = async (
   context: PRContext | IssueContext,
   handler: string,
   message: string
@@ -42,11 +49,11 @@ export const scheduleComment = (
     pendingComments.set(key, {
       debouncedPost: debounce(() => postComment(key), COMMENT_DEBOUNCE_TIME),
       context,
-      comments: []
+      comments: [],
     });
   }
 
   const pendingComment = pendingComments.get(key);
   pendingComment.comments.push({ handler, message });
-  pendingComment.debouncedPost();
+  await pendingComment.debouncedPost();
 };

pecker1115/probot-esphome

src/util/comment.ts
@@ -3,37 +3,44 @@
  *
  * We debounce it so that we only leave 1 comment with all notices.
  */
-import { debounce } from "debounce";
 import { COMMENT_DEBOUNCE_TIME } from "../const";
 import { PRContext, IssueContext, IssueOrPRContext } from "../types";
 import { getIssueFromPayload } from "./issue";
+import { debounce } from "./debounce";
 
 type PendingComment = {
-  debouncedPost: Function,
-  context: IssueOrPRContext,
-  comments: Array<{ handler: string, message: string }>;
-}
+  debouncedPost: Function;
+  context: IssueOrPRContext;
+  comments: Array<{ handler: string; message: string }>;
+};
 
 const pendingComments = new Map<string, PendingComment>();
 
-const postComment = (key: string) => {
+const postComment = async (key: string) => {
   const pendingComment = pendingComments.get(key);
   pendingComments.delete(key);
 
   const context = pendingComment.context;
 
-  const toPost = pendingComment.comments.map(
-    (comment) =>
-      `${comment.message}\n<sub><sup>(message by ${comment.handler})</sup></sub>`
+  // Group messages by handler in alphabetical order
+  const handlers = new Set(pendingComment.comments.map((c) => c.handler));
+  const toPost = [...handlers].sort().map(
+    (handler) =>
+      `${pendingComment.comments
+        .filter((comment) => comment.handler === handler)
+        .map((comment) => comment.message)
+        .join("\n")}\n` + `<sub><sup>(message by ${handler})</sup></sub>`
   );
 
-  let commentBody = toPost.join("\n\n---\n\n");
+  const commentBody = toPost.join("\n\n---\n\n");
 
-  context.octokit.issues.createComment(context.issue({ body: commentBody }));
+  await context.octokit.issues.createComment(
+    context.issue({ body: commentBody })
+  );
 };
 
-export const scheduleComment = (
-  context: any,
+export const scheduleComment = async (
+  context: IssueOrPRContext,
   handler: string,
   message: string
 ) => {
@@ -42,11 +49,11 @@ export const scheduleComment = (
     pendingComments.set(key, {
       debouncedPost: debounce(() => postComment(key), COMMENT_DEBOUNCE_TIME),
       context,
-      comments: []
+      comments: [],
     });
   }
 
   const pendingComment = pendingComments.get(key);
   pendingComment.comments.push({ handler, message });
-  pendingComment.debouncedPost();
+  await pendingComment.debouncedPost();
 };